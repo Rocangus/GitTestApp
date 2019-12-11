d4d85915cd251f1c19b83f8dfa9141f8874f8868
Rocangus <hunter_sniper112@hotmail.com>
2019-12-11 10:28:40 +0100

    Created car

---
+++ Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitTestApp.Models
+{
+    public class Car
+    {
+        [Key]
+        public string RegistrationNumber { get; set; }
+
+    }
+}
